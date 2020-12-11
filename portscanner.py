6aee5be13e048d891b3076acb201ccb8c5131967
Javier Nazario <42556353+Javier1019@users.noreply.github.com>
2020-12-11 02:54:31 -0400

    Port Scanner
    Added portscanner.py

---
+++ portscanner.py
@@ -0,0 +1,28 @@
+import socket as so
+import termcolor as color
+
+
+def scan(target, ports):
+	print('\n' + ' Starting Scan For ' + str(target))
+	for port in range(1,ports):
+		scan_port(target,port)
+
+
+def scan_port(ipaddress, port):
+	try:
+		sock = so.socket()
+		sock.connect((ipaddress, port))
+		print("[+] Port Opened " + str(port))
+		sock.close()
+	except:
+		pass
+
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ")
+ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
+if ',' in targets:
+	print(color.colored(("[*] Scanning Multiple Targets"), 'green'))
+	for ip_addr in targets.split(','):
+		scan(ip_addr.strip(' '), ports)
+else:
+	scan(targets,ports)
